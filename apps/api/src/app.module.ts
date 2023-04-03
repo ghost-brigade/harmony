970aa52a152c106a14765e09837fc418bbd9203c
Anthony ARJONA <anthony.arjona@outlook.com>
2023-04-03 21:59:37 +0200

    feat: add throttle

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,17 +1,27 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { MongooseModule } from "@nestjs/mongoose";
+import { ThrottlerModule, ThrottlerGuard } from "@nestjs/throttler";
 import { UserModule } from "./user/user.module";
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    ThrottlerModule.forRoot({
+      ttl: parseInt(process.env?.RATE_LIMIT_TTL || '1'),
+      limit: parseInt(process.env?.RATE_LIMIT_COUNT || '10'),
+    }),
     MongooseModule.forRoot(process.env.MONGODB_URI),
     UserModule
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: 'APP_GUARD',
+      useClass: ThrottlerGuard,
+    }
+  ],
 })
 
 export class AppModule {}
