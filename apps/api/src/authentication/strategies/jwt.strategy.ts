2f272f20aaa1d6fe02910f3f1472b04c73ad7f47
MoulinLouis <moulin.louis93@gmail.com>
2023-04-03 23:06:10 +0200

    add jwt strategies, guard and constants

---
+++ apps/api/src/authentication/strategies/jwt.strategy.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { jwtConstants } from '../constants/jwt.constants';
+import { AuthService } from '../auth.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor(private readonly authService: AuthService) {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            secretOrKey: jwtConstants.secret,
+        });
+    }
+
+    async validate(payload: { email: string }) {
+        return { email: payload.email };
+    }
+}
