df5e4fe39b7dc9734030f96a6f90b883fb0bea41
Anthony ARJONA <anthony.arjona@outlook.com>
2023-04-04 00:49:55 +0200

    feat: add zod guard in nest

---
+++ apps/api/src/core/guards/zod/zod.guard.ts
@@ -0,0 +1,29 @@
+import {
+    CanActivate,
+    ExecutionContext,
+    HttpException,
+    HttpStatus,
+    Injectable,
+  } from '@nestjs/common';
+  import { Observable } from 'rxjs';
+  import { ZodSchema } from 'zod';
+  
+  @Injectable()
+  export class ZodGuard implements CanActivate {
+    constructor(
+      private source: 'body' | 'query' | 'params',
+      private schema: ZodSchema,
+    ) {}
+    canActivate(
+      context: ExecutionContext,
+    ): boolean | Promise<boolean> | Observable<boolean> {
+      const data = context.switchToHttp().getRequest()[this.source];
+      const result = this.schema.safeParse(data);
+  
+      if (!result.success) {
+        throw new HttpException('Bad Request', HttpStatus.BAD_REQUEST);
+      }
+  
+      return true;
+    }
+  }
