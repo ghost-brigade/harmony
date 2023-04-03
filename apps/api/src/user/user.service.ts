0743f5be7c242b5c4ef77e429c77a452f73826c9
Anthony ARJONA <anthony.arjona@outlook.com>
2023-04-04 00:50:16 +0200

    feat: implement zod user schema

--- apps/api/src/user/user.service.ts
+++ apps/api/src/user/user.service.ts
@@ -1,32 +1,31 @@
 import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
-import { CreateUserDto } from "./dto/create-user.dto";
-import { User } from "../schemas";
-import { UpdateUserDto } from "./dto";
+import { createUserType, publicUserType, updateUserType } from "@harmony/zod";
 
 @Injectable()
 export class UserService {
     constructor(@InjectModel('User') private readonly userModel) {}
 
-    async create(createUserDto: CreateUserDto): Promise<User> {
-        const createdUser = new this.userModel(createUserDto);
+    async create(createUser: createUserType): Promise<publicUserType> {
+        const createdUser = new this.userModel(createUser);
         return await createdUser.save();
     }
 
-    async update(updateUserDto: UpdateUserDto): Promise<User> {
-        const updatedUser = new this.userModel(updateUserDto);
+    async update(updateUser: updateUserType): Promise<publicUserType> {
+        const updatedUser = new this.userModel(updateUser);
         return await updatedUser.save();
     }
 
-    async delete(id: string): Promise<User> {
-        return await this.userModel.findByIdAndRemove(id);
+    async delete(id: string): Promise<null> {
+        const deletedUser = await this.userModel.findByIdAndRemove(id).exec();
+        return await deletedUser;
     }
 
-    async findAll(): Promise<User[]> {
+    async findAll(): Promise<publicUserType[]> {
         return await this.userModel.find().exec();
     }
 
-    async findOne(id: string): Promise<User> {
+    async findOne(id: string): Promise<publicUserType> {
         return await this.userModel.findById(id).exec();
     }
 }
