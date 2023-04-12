3fc676eaf5b30985838380bf37e2c5dc9b56172a
Anthony ARJONA <anthony.arjona@outlook.com>
2023-04-12 23:15:39 +0200

    poc terraform

---
+++ terraform/modules/gke/variables.tf
@@ -0,0 +1,30 @@
+variable "env" {
+  type = "string"
+}
+
+variable "image_tag" {
+  type = "string"
+}
+
+variable "k8s_master_allowed_ip" {
+  type = "string"
+}
+
+variable "machine_type" {
+  default = "n1-standard-1"
+  type    = "string"
+}
+
+variable "network_name" {
+  type = "string"
+}
+
+variable "node_count" {
+  default = "1"
+  type    = "string"
+}
+
+variable "region" {
+  default = "europe-west9"
+  type = "string"
+}
