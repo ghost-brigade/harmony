a2d30367d3a0042cf05744bf370a9cf232946f03
Anthony ARJONA <anthony.arjona@outlook.com>
2023-04-14 22:16:50 +0200

    fix: try to fix pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,9 +22,9 @@ module "google_compute_network" {
 }
 
 module "google_compute_subnetwork" {
-  source            = "./modules/google_compute_subnetwork"
-  subnetwork_name   = "default"
-  vpc_self_link     = module.google_compute_network.vpc.self_link
+  source          = "./modules/google_compute_subnetwork"
+  subnetwork_name = "default"
+  vpc_self_link   = module.google_compute_network.vpc.self_link
 }
 
 module "google_container_cluster" {
@@ -38,32 +38,32 @@ module "google_container_cluster" {
 # ----------------------------------------
 
 provider "mongodbatlas" {
-  public_key = var.mongodbatlas_public_key
-  private_key  = var.mongodbatlas_private_key
+  public_key  = var.mongodbatlas_public_key
+  private_key = var.mongodbatlas_private_key
 }
 
 module "mongodbatlas_project" {
-  source = "./modules/mongodbatlas_project"
+  source       = "./modules/mongodbatlas_project"
   project_name = var.app_name
-  org_id = var.mongodbatlas_org_id
+  org_id       = var.mongodbatlas_org_id
 }
 
 module "mongodbatlas_project_ip_whitelist" {
-  source = "./modules/mongodbatlas_project_ip_whitelist"
-  project_id = module.mongodbatlas_project.project.id
+  source      = "./modules/mongodbatlas_project_ip_whitelist"
+  project_id  = module.mongodbatlas_project.project.id
   cidr_blocks = ["0.0.0.0/0"]
 }
 
 module "mongodbatlas_database_user" {
-  source = "./modules/mongodbatlas_database_user"
-  database_names = var.mongodbatlas_cluster_names
+  source            = "./modules/mongodbatlas_database_user"
+  database_names    = var.mongodbatlas_cluster_names
   database_password = var.mongodbatlas_database_password
-  role_name = "readWrite"
-  project_id = module.mongodbatlas_project.project.id
+  role_name         = "readWrite"
+  project_id        = module.mongodbatlas_project.project.id
 }
 
 module "mongodbatlas_cluster" {
-  source = "./modules/mongodbatlas_cluster"
+  source     = "./modules/mongodbatlas_cluster"
   project_id = module.mongodbatlas_project.project.id
-  names = var.mongodbatlas_cluster_names
+  names      = var.mongodbatlas_cluster_names
 }
