8fce6f84531cefa6b33314529c5210e17093099f
Anthony <anthony.arjona@outlook.com>
2023-04-17 22:37:16 +0200

    Fix/packages (#21)
    * fix: package deps
    
    * feat: add base of api

--- apps/service-gateway/src/gateway.module.ts
+++ apps/service-gateway/src/gateway.module.ts
@@ -1,14 +1,28 @@
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { ConfigModule } from '@nestjs/config';
 import { Module } from "@nestjs/common";
 import { AuthenticationModule } from "./authentication/authentication.module";
 import { AccountModule } from "./account/account.module";
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    ThrottlerModule.forRoot({
+      ttl: parseInt(process.env?.RATE_LIMIT_TTL || "1"),
+      limit: parseInt(process.env?.RATE_LIMIT_COUNT || "10"),
+    }),
     AuthenticationModule,
-    AccountModule
+    AccountModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: "GATEWAY_GUARD",
+      useClass: ThrottlerGuard,
+    },
+  ],
   exports: [],
 })
 export class GatewayModule {}
