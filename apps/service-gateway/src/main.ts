497bfde438f5d368d6b002340e7d6ed395244e0a
Anthony ARJONA <anthony.arjona@outlook.com>
2023-04-17 22:36:58 +0200

    feat: add base of api

--- apps/service-gateway/src/main.ts
+++ apps/service-gateway/src/main.ts
@@ -1,18 +1,33 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { Logger, VersioningType } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { GatewayModule } from "./gateway.module";
+import helmet from "helmet";
 
 async function bootstrap() {
   const app = await NestFactory.create(GatewayModule);
   const port = process.env.PORT || 3000;
+  const config = new DocumentBuilder()
+    .setTitle("Harmony API")
+    .setDescription("Harmony API")
+    .setVersion("1")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("docs", app, document);
+
+  app.use(helmet());
+  app.enableCors({ origin: [process.env?.CORS_ORIGIN || "*"] });
+  app.enableVersioning({
+    defaultVersion: "1",
+    type: VersioningType.HEADER,
+    header: "Accept-Version",
+  });
+
   await app.listen(port);
+
   Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/`
+    `🚀 Application is running on: http://localhost:${port}`
   );
 }
 
