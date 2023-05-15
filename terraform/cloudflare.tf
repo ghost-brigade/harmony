056de419e87aa002d51d3c81f6b60f610cc245c9
Anthony ARJONA <anthony.arjona@outlook.com>
2023-05-16 00:24:42 +0200

    fix: domain & proxie

--- terraform/cloudflare.tf
+++ terraform/cloudflare.tf
@@ -4,16 +4,16 @@ provider "cloudflare" {
 
 resource "cloudflare_record" "root" {
   zone_id = var.cloudflare_zone_id
-  name    = var.domain_name
+  name    = "@"
   type    = "A"
   value   = "8.8.8.8"
-  ttl     = 3600
+  proxied = true
 }
 
 resource "cloudflare_record" "www" {
   zone_id = var.cloudflare_zone_id
-  name    = "www.${var.domain_name}"
+  name    = "www"
   type    = "CNAME"
-  value   = var.domain_name
-  ttl     = 3600
+  value   = cloudflare_record.root.name
+  proxied = true
 }
