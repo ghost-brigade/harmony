0ebce2677fb8368f1609af0f2a220c2143431686
Anthony <anthony.arjona@outlook.com>
2023-05-16 00:15:52 +0200

    Feature/terraform cloudflare (#36)
    * feat: update CI to add cloudflare env
    
    * feat: add cloudflare provider
    
    * refact: split terraform config in multiple file
    
    * feat: update docker CI with custom path

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,5 @@
 terraform {
   backend "gcs" {
-    # Later inject data from CLI arguments, for now hardcode
     bucket = "harmony-terraform-bucket"
     prefix = "production"
   }
@@ -13,66 +12,9 @@ terraform {
       source  = "mongodb/mongodbatlas"
       version = "~> 0.9.0"
     }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 3.0"
+    }
   }
-}
-provider "google" {
-  project = var.google_project_id
-  region  = var.google_region
-}
-
-data "google_storage_bucket_object" "terraform_bucket" {
-  bucket = "harmony-terraform-bucket"
-}
-
-module "google_compute_network" {
-  source           = "./modules/google_compute_network"
-  vpc_network_name = var.app_name
-}
-
-module "google_compute_subnetwork" {
-  source          = "./modules/google_compute_subnetwork"
-  subnetwork_name = var.app_name
-  vpc_self_link   = module.google_compute_network.vpc.self_link
-}
-
-module "google_container_cluster" {
-  source           = "./modules/google_container_cluster"
-  cluster_name     = "${var.app_name}-gke"
-  region           = var.google_region
-  vpc_network_name = module.google_compute_network.vpc.name
-  subnetwork_name  = module.google_compute_subnetwork.subnet.name
-}
-
-# ----------------------------------------
-
-provider "mongodbatlas" {
-  public_key  = var.mongodbatlas_public_key
-  private_key = var.mongodbatlas_private_key
-}
-
-module "mongodbatlas_project" {
-  source       = "./modules/mongodbatlas_project"
-  project_name = var.app_name
-  org_id       = var.mongodbatlas_org_id
-}
-
-module "mongodbatlas_project_ip_whitelist" {
-  source      = "./modules/mongodbatlas_project_ip_whitelist"
-  project_id  = module.mongodbatlas_project.project.id
-  cidr_blocks = ["0.0.0.0/0"]
-}
-
-module "mongodbatlas_database_user" {
-  source            = "./modules/mongodbatlas_database_user"
-  database_names    = var.mongodbatlas_cluster_names
-  database_password = var.mongodbatlas_database_password
-  role_name         = "readWrite"
-  project_id        = module.mongodbatlas_project.project.id
-}
-
-module "mongodbatlas_cluster" {
-  source                      = "./modules/mongodbatlas_cluster"
-  project_id                  = module.mongodbatlas_project.project.id
-  names                       = var.mongodbatlas_cluster_names
-  provider_instance_size_name = "M2"
 }
