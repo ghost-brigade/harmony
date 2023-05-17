e2ff71017423f43cd65b165dde4b5754c20677b1
Anthony ARJONA <anthony.arjona@outlook.com>
2023-05-18 00:47:32 +0200

    feat: add message pattern for role

--- libs/service-config/src/message-pattern/role.enum.ts
+++ libs/service-config/src/message-pattern/role.enum.ts
@@ -1,5 +1,11 @@
 export enum ROLE_MESSAGE_PATTERN {
+  FIND_ALL = "role.find.all",
+  FIND_ONE = "role.find.one",
   CREATE = "role.create",
   UPDATE = "role.update",
   DELETE = "role.delete",
+  ADD_USER = "role.add.user",
+  REMOVE_USER = "role.remove.user",
+  ADD_PERMISSION = "role.add.permission",
+  REMOVE_PERMISSION = "role.remove.permission",
 };
