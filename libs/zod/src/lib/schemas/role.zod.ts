004e8a267c337fbdbb48d28fb5edf329cf45d9bf
Anthony ARJONA <anthony.arjona@outlook.com>
2023-05-18 00:48:12 +0200

    feat: add role zod Schema/Type/Dto

--- libs/zod/src/lib/schemas/role.zod.ts
+++ libs/zod/src/lib/schemas/role.zod.ts
@@ -2,18 +2,42 @@ import { z } from "zod";
 import { IdSchema } from "./id.zod";
 import { createZodDto } from "nestjs-zod";
 
+export const RoleUserSchema = z.object({
+  users: z.array(IdSchema),
+});
+
+export const RolePermissionSchema = z.object({
+  permissions: z.array(IdSchema),
+});
+
 export const RoleSchema = z.object({
   id: IdSchema,
   server: IdSchema,
   name: z.string(),
-  permissions: z.array(z.string()).optional(),
-  users: z.array(IdSchema).optional(),
+  permissions: RolePermissionSchema.optional(),
+  users: RoleUserSchema.optional(),
 });
 
+export const RoleParamsSchema = RoleSchema.pick({
+  server: true,
+  name: true,
+}).partial();
+
 export const RoleCreateSchema = RoleSchema.omit({ id: true });
+export const RoleUpdateSchema = RoleSchema.pick({ name: true }).required();
+
+export type RoleUserType = z.infer<typeof RoleUserSchema>;
+export type RolePermissionType = z.infer<typeof RolePermissionSchema>;
 
 export type RoleType = z.infer<typeof RoleSchema>;
 export type RoleCreateType = z.infer<typeof RoleCreateSchema>;
+export type RoleUpdateType = z.infer<typeof RoleUpdateSchema>;
+export type RoleParamsType = z.infer<typeof RoleParamsSchema>;
+
+export class RoleUserDto extends createZodDto(RoleUserSchema) {}
+export class RolePermissionDto extends createZodDto(RolePermissionSchema) {}
 
 export class RoleDto extends createZodDto(RoleSchema) {}
 export class RoleCreateDto extends createZodDto(RoleCreateSchema) {}
+export class RoleUpdateDto extends createZodDto(RoleUpdateSchema) {}
+export class RoleParamsDto extends createZodDto(RoleParamsSchema) {}
