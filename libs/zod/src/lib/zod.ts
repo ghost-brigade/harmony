ee3885eaeee728bdde2171ebed4759c623172b7d
Anthony ARJONA <anthony.arjona@outlook.com>
2023-05-18 00:47:54 +0200

    feat: export IdSchema from zod lib

--- libs/zod/src/lib/zod.ts
+++ libs/zod/src/lib/zod.ts
@@ -3,6 +3,7 @@ export * from "./schemas/user/user.zod";
 export * from "./schemas/user/userJwt.zod";
 export * from "./schemas/server.zod";
 export * from "./schemas/role.zod";
+export * from "./schemas/id.zod";
 
 export * from "./format/error";
 
