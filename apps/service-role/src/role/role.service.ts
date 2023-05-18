ff171e52a0d1311fcac7b0ac8315a622d568e92b
Anthony ARJONA <anthony.arjona@outlook.com>
2023-05-18 13:37:18 +0200

    feat: create a verify user endpoint

--- apps/service-role/src/role/role.service.ts
+++ apps/service-role/src/role/role.service.ts
@@ -1,22 +1,34 @@
 import {
+  ACCOUNT_MESSAGE_PATTERN,
+  Services,
+  getServiceProperty,
+} from "@harmony/service-config";
+import {
   FormatZodResponse,
   RoleCreateSchema,
   RoleCreateType,
   IdType,
   RoleType,
-  RoleSchema
+  RoleSchema,
+  RoleUserType,
 } from "@harmony/zod";
 import {
   BadRequestException,
+  Inject,
   Injectable,
   InternalServerErrorException,
 } from "@nestjs/common";
-import { RpcException } from "@nestjs/microservices";
+import { ClientProxy, RpcException } from "@nestjs/microservices";
 import { InjectModel } from "@nestjs/mongoose";
+import { Observable, firstValueFrom } from "rxjs";
 
 @Injectable()
 export class RoleService {
-  constructor(@InjectModel("Role") private readonly roleModel) {}
+  constructor(
+    @InjectModel("Role") private readonly roleModel,
+    @Inject(getServiceProperty(Services.ACCOUNT, "name"))
+    private readonly client: ClientProxy
+  ) {}
 
   async createRole({ role, user }: { role: RoleCreateType; user: any }) {
     const parse = RoleCreateSchema.safeParse(role);
@@ -27,7 +39,7 @@ export class RoleService {
       );
     }
 
-    const { name, server } = parse.data;
+    const { name, server, permissions, users } = parse.data;
 
     const isRoleExist = await this.getRoleByNameAndServerId({ name, server });
     if (isRoleExist) {
@@ -38,8 +50,28 @@ export class RoleService {
       );
     }
 
+    if (users) {
+      const verifyUsers = await this.verifiyUsers({ users });
+      if (!verifyUsers) {
+        throw new RpcException(
+          new BadRequestException(`At least one user does not exist`)
+        );
+      }
+    }
+
     const newRole = new this.roleModel(role);
-    return RoleSchema.parse((await newRole.save()) as RoleType);
+    //return RoleSchema.parse((await newRole.save()) as RoleType);
+    return newRole;
+  }
+
+  private async verifiyUsers({
+    users,
+  }: {
+    users: RoleUserType;
+  }): Promise<boolean> {
+    return await firstValueFrom(
+      this.client.send(ACCOUNT_MESSAGE_PATTERN.VERIFY_USERS, users)
+    );
   }
 
   private async getRoleByNameAndServerId({
