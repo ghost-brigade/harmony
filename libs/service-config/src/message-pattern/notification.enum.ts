877bea77d4b8e21cf80ce5800ef10a821cf45585
Anthony ARJONA <anthony.arjona@outlook.com>
2023-07-01 03:24:53 +0200

    feat: add endpoint for socket

--- libs/service-config/src/message-pattern/notification.enum.ts
+++ libs/service-config/src/message-pattern/notification.enum.ts
@@ -1,3 +1,5 @@
 export enum NOTIFICATION_MESSAGE_PATTERN {
   NEW_MESSAGE = "notification.new_message",
+  UPDATE_MESSAGE = "notification.update_message",
+  DELETE_MESSAGE = "notification.delete_message",
 }
