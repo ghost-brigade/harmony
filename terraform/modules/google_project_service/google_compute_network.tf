873e1d015adb18fb3461bfadebd2962781048c98
Anthony ARJONA <anthony.arjona@outlook.com>
2023-07-03 23:24:20 +0200

    fix: tf operand

--- terraform/modules/google_project_service/google_compute_network.tf
+++ terraform/modules/google_project_service/google_compute_network.tf
@@ -6,5 +6,5 @@ resource "google_project_service" "project_services" {
   for_each = local.services
 
   project = var.project_id
-  service = each.key + ".googleapis.com"
+  service = "${each.key}.googleapis.com"
 }
