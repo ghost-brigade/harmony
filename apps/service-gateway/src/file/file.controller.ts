83623ec9ae47443b899f3579a428cbbf5a610727
MoulinLouis <moulin.louis93@gmail.com>
2023-07-05 00:37:08 +0200

    fix(file): remove all endpoints

--- apps/service-gateway/src/file/file.controller.ts
+++ apps/service-gateway/src/file/file.controller.ts
@@ -41,70 +41,70 @@ export class FileController {
     private readonly serviceRequest: ServiceRequest
   ) {}
 
-  @ApiOperation({ summary: "Get all files" })
-  @ApiOkResponse({ status: 200, description: "Return all files" })
-  @ApiBadRequestResponse({ status: 400, description: "Bad request" })
-  @ApiUnauthorizedResponse({ description: "Unauthorized" })
-  @Get()
-  async findAll() {
-    return this.serviceRequest.send({
-      client: this.client,
-      pattern: FILE_MESSAGE_PATTERN.FIND_ALL,
-    });
-  }
+  // @ApiOperation({ summary: "Get all files" })
+  // @ApiOkResponse({ status: 200, description: "Return all files" })
+  // @ApiBadRequestResponse({ status: 400, description: "Bad request" })
+  // @ApiUnauthorizedResponse({ description: "Unauthorized" })
+  // @Get()
+  // async findAll() {
+  //   return this.serviceRequest.send({
+  //     client: this.client,
+  //     pattern: FILE_MESSAGE_PATTERN.FIND_ALL,
+  //   });
+  // }
 
-  @ApiOperation({ summary: "Get file by id" })
-  @ApiOkResponse({
-    status: 200,
-    description: "Return file by id",
-    type: FileCreateDto,
-  })
-  @ApiBadRequestResponse({ status: 400, description: "Bad request" })
-  @ApiUnauthorizedResponse({ description: "Unauthorized" })
-  @Get(":id")
-  async findById(@Param("id") id: string) {
-    return this.serviceRequest.send({
-      client: this.client,
-      pattern: FILE_MESSAGE_PATTERN.FIND_BY_ID,
-      data: { id },
-    });
-  }
+  // @ApiOperation({ summary: "Get file by id" })
+  // @ApiOkResponse({
+  //   status: 200,
+  //   description: "Return file by id",
+  //   type: FileCreateDto,
+  // })
+  // @ApiBadRequestResponse({ status: 400, description: "Bad request" })
+  // @ApiUnauthorizedResponse({ description: "Unauthorized" })
+  // @Get(":id")
+  // async findById(@Param("id") id: string) {
+  //   return this.serviceRequest.send({
+  //     client: this.client,
+  //     pattern: FILE_MESSAGE_PATTERN.FIND_BY_ID,
+  //     data: { id },
+  //   });
+  // }
 
-  @ApiOperation({ summary: "Create file" })
-  @ApiCreatedResponse({
-    status: 201,
-    description: "Return created file",
-    type: FileDto,
-  })
-  @ApiBadRequestResponse({ status: 400, description: "Bad request" })
-  @ApiUnauthorizedResponse({ description: "Unauthorized" })
-  @UseInterceptors(FileInterceptor("file"))
-  @Post()
-  async create(
-    @UploadedFile() file: Express.Multer.File,
-    @Body() FileCreateDto: FileCreateDto
-  ) {
-    return this.serviceRequest.send({
-      client: this.client,
-      pattern: FILE_MESSAGE_PATTERN.CREATE,
-      data: {
-        ...FileCreateDto,
-        file,
-      },
-    });
-  }
+  // @ApiOperation({ summary: "Create file" })
+  // @ApiCreatedResponse({
+  //   status: 201,
+  //   description: "Return created file",
+  //   type: FileDto,
+  // })
+  // @ApiBadRequestResponse({ status: 400, description: "Bad request" })
+  // @ApiUnauthorizedResponse({ description: "Unauthorized" })
+  // @UseInterceptors(FileInterceptor("file"))
+  // @Post()
+  // async create(
+  //   @UploadedFile() file: Express.Multer.File,
+  //   @Body() FileCreateDto: FileCreateDto
+  // ) {
+  //   return this.serviceRequest.send({
+  //     client: this.client,
+  //     pattern: FILE_MESSAGE_PATTERN.CREATE,
+  //     data: {
+  //       ...FileCreateDto,
+  //       file,
+  //     },
+  //   });
+  // }
 
-  @ApiOperation({ summary: "Delete file" })
-  @ApiNoContentResponse({ status: 204, description: "Return deleted file" })
-  @ApiBadRequestResponse({ status: 400, description: "Bad request" })
-  @ApiUnauthorizedResponse({ description: "Unauthorized" })
-  @HttpCode(204)
-  @Delete(":id")
-  async delete(@Param("id") id: string) {
-    return this.serviceRequest.send({
-      client: this.client,
-      pattern: FILE_MESSAGE_PATTERN.DELETE,
-      data: { id },
-    });
-  }
+  // @ApiOperation({ summary: "Delete file" })
+  // @ApiNoContentResponse({ status: 204, description: "Return deleted file" })
+  // @ApiBadRequestResponse({ status: 400, description: "Bad request" })
+  // @ApiUnauthorizedResponse({ description: "Unauthorized" })
+  // @HttpCode(204)
+  // @Delete(":id")
+  // async delete(@Param("id") id: string) {
+  //   return this.serviceRequest.send({
+  //     client: this.client,
+  //     pattern: FILE_MESSAGE_PATTERN.DELETE,
+  //     data: { id },
+  //   });
+  // }
 }
