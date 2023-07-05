0f4d54fd985e7043388a6d08e330aa34d94f7653
MoulinLouis <moulin.louis93@gmail.com>
2023-07-05 11:39:10 +0200

    feat: add author datas on get messages by channel

---
+++ apps/service-message/src/message/interceptors/global-all-message.interceptor.ts
@@ -0,0 +1,76 @@
+import { ServiceRequest } from "@harmony/nest-microservice";
+import {
+  ACCOUNT_MESSAGE_PATTERN,
+  FILE_MESSAGE_PATTERN,
+  ROLE_MESSAGE_PATTERN,
+  Services,
+  getServiceProperty,
+} from "@harmony/service-config";
+import {
+  MessageType,
+  RoleType,
+  ServerType,
+  UserPublicType,
+} from "@harmony/zod";
+import {
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+  Injectable,
+  Inject,
+} from "@nestjs/common";
+import { ClientProxy } from "@nestjs/microservices";
+import { from, Observable } from "rxjs";
+import { switchMap, map } from "rxjs/operators";
+
+@Injectable()
+export class GlobalAllMessageInterceptor implements NestInterceptor {
+  constructor(
+    @Inject(getServiceProperty(Services.ACCOUNT, "name"))
+    private readonly accountService: ClientProxy,
+    @Inject(getServiceProperty(Services.FILE, "name"))
+    private readonly fileService: ClientProxy,
+    private readonly serviceRequest: ServiceRequest
+  ) {}
+
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      switchMap((response) => {
+        const messages = response.messages;
+        const updatedMessages = messages.map((message) => this.global(message));
+
+        return from(Promise.all(updatedMessages)).pipe(
+          map((processedMessages) => ({
+            ...response,
+            messages: processedMessages,
+          }))
+        );
+      })
+    );
+  }
+
+  private async global(message: MessageType) {
+    const aggregateObject = {
+      author: null,
+    };
+
+    if (message.author) {
+      aggregateObject.author = await this.authorMessage(message);
+    }
+
+    // @ts-ignore
+    return Object.assign(message.toJSON(), aggregateObject);
+  }
+
+  private async authorMessage(message: MessageType) {
+    const author = await this.serviceRequest.send({
+      client: this.accountService,
+      pattern: ACCOUNT_MESSAGE_PATTERN.FIND_ONE,
+      data: {
+        id: message.author,
+      },
+      promise: true,
+    });
+    return author;
+  }
+}
