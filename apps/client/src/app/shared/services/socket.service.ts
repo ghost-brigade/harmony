8b2c88fb6f3288b21908ed3d0b613b9fdc5b9488
Alexis LOURS <alexislours@protonmail.com>
2023-07-06 14:02:45 +0200

    feat: add streams

--- apps/client/src/app/shared/services/socket.service.ts
+++ apps/client/src/app/shared/services/socket.service.ts
@@ -20,6 +20,7 @@ export class SocketService {
   constructor() {
     this.messageSocket.on("disconnect", () => {
       this.messageSocket.connect();
+      this.messageSocket.emit("RECONNECT", "RECONNECT");
     });
   }
   /*
